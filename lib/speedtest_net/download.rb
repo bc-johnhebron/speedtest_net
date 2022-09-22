d8d862336c6808bae0337c45bef91b586d434da5
John Hebron <johnhebron@gmail.com>
2022-09-22 01:36:42 -0500

    again

--- lib/speedtest_net/download.rb
+++ lib/speedtest_net/download.rb
@@ -10,7 +10,7 @@ require 'speedtest_net/http_timeout'
 module SpeedtestNet
   class Download
     FILES = %w[random350x350.jpg random500x500.jpg random1000x1000.jpg
-               random1500x1500.jpg].freeze
+               random1500x1500.jpg random2000x2000.jpg random4000x4000.jpg].freeze
 
     def initialize(results)
       @results = results
